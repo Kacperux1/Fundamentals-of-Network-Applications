dbdd8944a1ce09324c22479a4ffcd755ef2c40b1
Jędrzej <251482@edu.p.lodz.pl>
2025-12-28 21:41:46 +0100

    dodawanie nowego facility i początki update

---
+++ src/main/java/pl/facility_rental/facility/mvc/FacilityRestClient.java
@@ -0,0 +1,50 @@
+package pl.facility_rental.facility.mvc;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import pl.facility_rental.facility.dto.CreateFacilityDto;
+import pl.facility_rental.facility.dto.ReturnedFacilityDto;
+
+import java.util.List;
+
+@Service
+public class FacilityRestClient {
+    private final WebClient webClient;
+
+    public FacilityRestClient(WebClient facilityWebClient) {
+        this.webClient = facilityWebClient;
+    }
+
+    public List<ReturnedFacilityDto> getAll() {
+        return webClient.get()
+                .retrieve()
+                .bodyToFlux(ReturnedFacilityDto.class)
+                .collectList()
+                .block();
+    }
+
+    public void deleteById(String id) {
+        webClient.delete()
+                .uri("/{id}", id)
+                .retrieve()
+                .bodyToMono(Void.class)
+                .block();
+    }
+
+    public ReturnedFacilityDto addFacility(CreateFacilityDto facility){
+        return webClient.post()
+                .bodyValue(facility)
+                .retrieve()
+                .bodyToMono(ReturnedFacilityDto.class)
+                .block();
+    }
+
+    public ReturnedFacilityDto getById(String id) {
+        return webClient
+                .get()
+                .uri("/facilities/{id}", id)
+                .retrieve()
+                .bodyToMono(ReturnedFacilityDto.class)
+                .block();
+    }
+}
---
+++ src/main/java/pl/facility_rental/facility/mvc/FacilityViewController.java
@@ -0,0 +1,60 @@
+package pl.facility_rental.facility.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import pl.facility_rental.facility.dto.CreateFacilityDto;
+import pl.facility_rental.facility.dto.ReturnedFacilityDto;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("view/facilities")
+public class FacilityViewController {
+
+    private final FacilityRestClient facilityClient;
+
+    public FacilityViewController(FacilityRestClient facilityClient) {
+        this.facilityClient = facilityClient;
+    }
+
+    @GetMapping
+    public String getAllFacilities(Model model) {
+        model.addAttribute("facilities", facilityClient.getAll());
+        return "facilityList";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String deleteFacility(@PathVariable String id,
+                                 RedirectAttributes redirectAttributes) {
+        try {
+            facilityClient.deleteById(id);
+            redirectAttributes.addFlashAttribute("Message", "Usunięto obiekt");
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute(
+                    "Message",
+                    "Nie można usunąć obiektu"
+            );
+        }
+        return "redirect:/view/facilities";
+    }
+
+    @GetMapping("/add")
+    public String getAddFacility(Model model) {
+        return "addFacility";
+    }
+
+    @PostMapping("/add")
+    public ReturnedFacilityDto addFacility(CreateFacilityDto facility) {
+        ReturnedFacilityDto newFacility = facilityClient.addFacility(facility);
+        return newFacility;
+    }
+
+    @GetMapping("update/{id}")
+    public String getFacilityDetails(@PathVariable String id, Model model) {
+        ReturnedFacilityDto facility = facilityClient.getById(id);
+        model.addAttribute("facility", facility);
+        return "updateFacility";
+    }
+}
